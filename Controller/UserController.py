fce4f24687c65e30786eb85bb0faccd3c80fe9a4
mahmutcant <mahcan012@gmail.com>
2023-08-14 21:02:31 +0300

    Cors settings done

--- Controller/UserController.py
+++ Controller/UserController.py
@@ -17,15 +17,17 @@ def addUser():
     username = data.get('username')
     password = data.get('password')
     email = data.get('email')
-    if not username or not password or not email:
-        return jsonify({"message":"Lütfen tüm alanları doldurun"}), 400
     yeni_kullanici = User(username=username,password=password,email=email)
-    db.session.add(yeni_kullanici)
-    db.session.commit()
-    grandparent_directory = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    users_folder_path = os.path.join(grandparent_directory, 'Users', username)
-    os.makedirs(users_folder_path, exist_ok=True)
-    return jsonify({'message':'Kayıt başarıyla oluşturuldu'}), 200
+    try:
+        db.session.add(yeni_kullanici)
+        db.session.commit()
+        grandparent_directory = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        users_folder_path = os.path.join(grandparent_directory, 'Users', username)
+        os.makedirs(users_folder_path, exist_ok=True)
+        return jsonify({'message':'Kayıt başarıyla oluşturuldu'}), 200
+    except:
+        return jsonify({'message':'Başarısız'}), 500
+    
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS, cross_origin
 secretKey = "XBEN6YhtZzICrrS34mNAPjgg6SlYMofx"
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SECRET_KEY'] = secretKey
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123qwe@localhost:5432/projects'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
